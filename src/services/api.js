fe7b4815e38413543fe746b5ec3c3b02a6381c95
Jayasri1171 <jayasridurgaperumalla@gmail.com>
2026-03-17 21:04:35 +0530

    added deployed backend url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api" 
+  baseURL: "https://pendrive-backend-h0n3.onrender.com/api/" 
 });
 
 // Register pendrive (admin / testing)
